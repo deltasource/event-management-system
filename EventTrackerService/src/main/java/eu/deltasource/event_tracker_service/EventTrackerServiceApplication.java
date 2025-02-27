0c1cc65244d7ac60c8e9a3952a04a79f26f13670
Anton Veselinov Urdzhanov <antonveselinovurdzhanov@Antons-MBP.deltasource.eu>
2025-02-27 15:24:14 +0200

    Create event tracker microservice and dockerizing it

---
+++ EventTrackerService/src/main/java/eu/deltasource/event_tracker_service/EventTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.deltasource.event_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ EventTrackerService/src/test/java/eu/deltasource/event_tracker_service/EventTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.deltasource.event_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
