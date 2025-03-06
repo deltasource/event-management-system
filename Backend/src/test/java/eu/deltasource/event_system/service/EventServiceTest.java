42b66ab13bc558ad28c9fcd7822e53b6a4090424
Anton Veselinov Urdzhanov <antonveselinovurdzhanov@Antons-MBP.deltasource.eu>
2025-03-06 15:00:09 +0200

    Replace mocked event service tests with Testcontainers

--- Backend/src/test/java/eu/deltasource/event_system/service/EventServiceTest.java
+++ Backend/src/test/java/eu/deltasource/event_system/service/EventServiceTest.java
@@ -1,136 +1,151 @@
 package eu.deltasource.event_system.service;
 
-import eu.deltasource.EntityMapper;
+import eu.deltasource.dto.AttendeeDto;
 import eu.deltasource.dto.CreateEventDto;
 import eu.deltasource.dto.EventDto;
-import eu.deltasource.event_system.exceptions.EventNotFoundException;
 import eu.deltasource.event_system.model.Event;
-import eu.deltasource.event_system.repository.AttendeeRepository;
 import eu.deltasource.event_system.repository.EventRepository;
-import jakarta.validation.Validator;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Testcontainers
 public class EventServiceTest {
-    @Mock
-    private EventRepository eventRepository;
-    @Mock
-    private AttendeeRepository attendeeRepository;
-    @Mock
-    private EntityMapper entityMapper;
-    @Mock
-    private Validator validator;
-    @InjectMocks
+
+    @Autowired
     private EventService eventService;
+    @Autowired
+    private EventRepository eventRepository;
+
+    @ServiceConnection
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:14");
 
     @Test
-    public void getEvents() {
-        //Given
-        Event event = new Event(UUID.randomUUID(), "Event", LocalDateTime.now(), "venue", 100, "details", 10, new ArrayList<>());
-        EventDto eventViewDto = new EventDto(event.getId(), event.getName(), event.getVenue(), event.getDateTime().toString(), event.getMaxCapacity(), event.getOrganizerDetails(), event.getTicketPrice());
-        List<Event> events = List.of(event);
-        when(eventRepository.findAll())
-                .thenReturn(events);
-        when(entityMapper.mapFromTo(any(Event.class), eq(EventDto.class)))
-                .thenReturn(eventViewDto);
-
-        //When
-        List<EventDto> allEvents = eventService.getAllEvents();
-
-        //Then
-        assertEquals(events.size(), allEvents.size());
-        assertEquals(events.get(0).getName(), allEvents.get(0).getName());
+    void connectionEstablished() {
+        assertTrue(postgreSQLContainer.isCreated());
+        assertTrue(postgreSQLContainer.isRunning());
     }
 
     @Test
-    public void createEventSuccessfully() {
+    @Transactional
+    void getAllEvents_Successfully() {
+        //When
+        List<EventDto> fetchedEvents = eventService.getAllEvents();
+
+        //Then
+        assertFalse(fetchedEvents.isEmpty());
+    }
+
+    @Test
+    void createEvent_Successfully() {
         //Given
         CreateEventDto createEventDto = new CreateEventDto("Event", LocalDateTime.now().toString(), "venue", 100, "details", 10);
-        Event event = new Event(UUID.randomUUID(), "Event", LocalDateTime.parse(createEventDto.dateTime()), "venue", 100, "details", 10, new ArrayList<>());
-        when(entityMapper.mapFromTo(createEventDto, Event.class))
-                .thenReturn(event);
-        when(validator.validate(event))
-                .thenReturn(Set.of());
 
         //When
+        String eventName = eventService.create(createEventDto);
+
+        //Then
+        assertEquals(createEventDto.name(), eventName);
+        Optional<Event> optionalEvent = eventRepository.findAll().stream()
+                .filter(e -> e.getName().equals("Event"))
+                .findFirst();
+        if (optionalEvent.isPresent()) {
+            Event savedEvent = optionalEvent.get();
+            assertEquals(savedEvent.getName(), createEventDto.name());
+            assertEquals(savedEvent.getDateTime().toString(), createEventDto.dateTime());
+            assertEquals(savedEvent.getVenue(), createEventDto.venue());
+            assertEquals(savedEvent.getMaxCapacity(), createEventDto.maxCapacity());
+            assertEquals(savedEvent.getOrganizerDetails(), createEventDto.organizerDetails());
+            assertEquals(savedEvent.getTicketPrice(), createEventDto.ticketPrice());
+        }
+    }
+
+    @Test
+    @Transactional
+    void deleteEvent_Successfully() {
+        //Given
+        CreateEventDto createEventDto = new CreateEventDto("Event", LocalDateTime.now().toString(), "venue", 100, "details", 10);
         eventService.create(createEventDto);
-
-        //Then
-        ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
-        verify(eventRepository).save(eventArgumentCaptor.capture());
-        Event savedEvent = eventArgumentCaptor.getValue();
-        assertEquals(event, savedEvent);
-    }
-
-    @Test
-    public void deleteEventSuccessfully() {
-        //Given
-        Event event = new Event(UUID.randomUUID(), "Event", LocalDateTime.now(), "venue", 100, "details", 10, new ArrayList<>());
-        UUID uuid = UUID.randomUUID();
-        when(eventRepository.findById(uuid))
-                .thenReturn(Optional.of(event));
+        Optional<Event> optionalEvent = eventRepository.findAll().stream()
+                .filter(e -> e.getName().equals("Event"))
+                .findFirst();
+        UUID eventId = null;
+        if (optionalEvent.isPresent()) {
+            eventId = optionalEvent.get().getId();
+        }
 
         //When
-        eventService.delete(uuid);
+        eventService.delete(eventId);
 
         //Then
-        verify(eventRepository, times(1)).delete(event);
-        verify(attendeeRepository, times(1)).deleteAll(event.getAttendees());
+        assertNotNull(eventId);
+        assertTrue(eventRepository.findById(eventId).isEmpty());
     }
 
     @Test
-    public void deleteEventFails_whenEventIsNotFound() {
+    void updateEvent_Successfully() {
         //Given
-        UUID uuid = UUID.randomUUID();
-        when(eventRepository.findById(uuid))
-                .thenReturn(Optional.empty());
+        CreateEventDto createEventDto = new CreateEventDto("Event", LocalDateTime.now().toString(), "venue", 100, "details", 10);
+        CreateEventDto updateDto = new CreateEventDto("UpdatedEvent", LocalDateTime.now().toString(), "venue", 100, "details", 10);
+        eventService.create(createEventDto);
+        Optional<Event> optionalEvent = eventRepository.findAll().stream()
+                .filter(e -> e.getName().equals("Event"))
+                .findFirst();
+        UUID eventId = null;
+        if (optionalEvent.isPresent()) {
+            eventId = optionalEvent.get().getId();
+        }
 
-        //When, Then
-        assertThrows(EventNotFoundException.class, () -> {
-            eventService.delete(uuid);
-        });
+        //When
+        eventService.updateEvent(eventId, updateDto);
+
+        //Then
+        assertNotNull(eventId);
+        assertFalse(eventRepository.findById(eventId).isEmpty());
+        assertEquals(updateDto.name(), eventRepository.findById(eventId).get().getName());
     }
 
     @Test
-    public void updateEventSuccessfully() throws IOException {
-        // Given
-        UUID uuid = UUID.randomUUID();
-        LocalDateTime dateTime = LocalDateTime.now();
-        CreateEventDto createEventDto = new CreateEventDto("Event1", dateTime.toString(), "venue", 100, "details", 10);
-        Event event = new Event(uuid, "Event", dateTime, "venue", 100, "details", 10, new ArrayList<>());
-        Event updatedEvent = new Event(uuid, "Event1", dateTime, "venue", 100, "details", 10, new ArrayList<>());
-        when(eventRepository.findById(uuid))
-                .thenReturn(Optional.of(event));
-        when(entityMapper.mapFromTo(createEventDto, event))
-                .thenReturn(updatedEvent);
-        lenient().when(validator.validate(updatedEvent))
-                .thenReturn(Set.of());
+    @Transactional
+    void getAttendeesOfEvent_Successfully() {
+        //Given
+        CreateEventDto createEventDto = new CreateEventDto("Event", LocalDateTime.now().toString(), "venue", 100, "details", 10);
+        eventService.create(createEventDto);
+        Optional<Event> optionalEvent = eventRepository.findAll().stream()
+                .filter(e -> e.getName().equals("Event"))
+                .findFirst();
+        UUID eventId = null;
+        if (optionalEvent.isPresent()) {
+            eventId = optionalEvent.get().getId();
+        }
+        AttendeeDto attendeeToAdd = new AttendeeDto(null, "John", "Smith", "john@gmail.com", "VIP");
+        eventService.addAttendeeToEvent(eventId, attendeeToAdd);
 
-        // When
-        eventService.updateEvent(uuid, createEventDto);
+        //When
+        List<AttendeeDto> attendees = eventService.getAllByEvent(eventId);
 
-        // Then
-        assertEquals(createEventDto.name(), updatedEvent.getName());
-        assertEquals(createEventDto.maxCapacity(), updatedEvent.getMaxCapacity());
-        assertEquals(createEventDto.venue(), updatedEvent.getVenue());
-        assertEquals(LocalDateTime.parse(createEventDto.dateTime()), updatedEvent.getDateTime());
-        assertEquals(createEventDto.organizerDetails(), updatedEvent.getOrganizerDetails());
-        verify(eventRepository, times(1)).save(updatedEvent);
+        //Then
+        AttendeeDto fetchedAttendee = attendees.get(0);
+        assertNotNull(eventId);
+        assertNotNull(attendees);
+        assertEquals(1, attendees.size());
+        assertEquals(attendeeToAdd.firstName(), fetchedAttendee.firstName());
+        assertEquals(attendeeToAdd.lastName(), fetchedAttendee.lastName());
+        assertEquals(attendeeToAdd.email(), fetchedAttendee.email());
+        assertEquals(attendeeToAdd.ticketType(), fetchedAttendee.ticketType());
     }
-
 }
